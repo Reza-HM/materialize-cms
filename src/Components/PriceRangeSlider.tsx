02d4f6b01d8ca0641a07c2ccdfaccbd5996100b2
Reza Hamzei <hamzeiamir205@gmail.com>
2023-12-20 12:02:58 +0330

    refactoring the price range slider

--- src/Components/PriceRangeSlider.tsx
+++ src/Components/PriceRangeSlider.tsx
@@ -1,46 +1,108 @@
-import React, { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const PriceRangeSlider: React.FC = () => {
   const [minPrice, setMinPrice] = useState<number>(0);
-  const [maxPrice, setMaxPrice] = useState<number>(100);
-  const [sliderValue, setSliderValue] = useState<number[]>([0, 100]);
+  const [maxPrice, setMaxPrice] = useState<number>(1000);
+  const [sliderWidth, setSliderWidth] = useState<number>(0);
+  const [dragging, setDragging] = useState<boolean>(false);
+  const sliderRef = useRef<HTMLDivElement>(null);
 
-  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newSliderValue = Number(event.target.value);
-    const midPoint = (minPrice + maxPrice) / 2;
+  useEffect(() => {
+    if (sliderRef.current) {
+      setSliderWidth(sliderRef.current.offsetWidth);
+    }
+  }, []);
 
-    if (newSliderValue < midPoint) {
-      setMinPrice(newSliderValue);
-      setSliderValue([newSliderValue, maxPrice]);
-    } else {
-      setMaxPrice(newSliderValue);
-      setSliderValue([minPrice, newSliderValue]);
+  const handleMinChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newMinPrice = parseInt(event.target.value);
+    if (newMinPrice < 1001) {
+      setMinPrice(newMinPrice);
     }
   };
 
+  const handleMaxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newMaxPrice = parseInt(event.target.value);
+    if (newMaxPrice >= minPrice) {
+      setMaxPrice(newMaxPrice);
+      if (minPrice > newMaxPrice) {
+        setMinPrice(newMaxPrice);
+      }
+    } else {
+      setMaxPrice(minPrice);
+    }
+  };
+
+  const handleSliderMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setDragging(true);
+  };
+
+  const handleSliderMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (dragging && sliderRef.current) {
+      const offsetX =
+        event.pageX - sliderRef.current.getBoundingClientRect().left;
+      let newMinPrice = Math.floor((offsetX / sliderWidth) * 1000);
+
+      if (newMinPrice < 0) {
+        newMinPrice = 0;
+      } else if (newMinPrice > maxPrice) {
+        newMinPrice = maxPrice;
+      }
+
+      setMinPrice(newMinPrice);
+    }
+  };
+
+  const handleSliderMouseUp = () => {
+    setDragging(false);
+  };
+
   return (
-    <div className="w-full max-w-md mx-auto mt-6">
-      <div className="flex justify-between">
-        <span>${minPrice}</span>
-        <span>${maxPrice}</span>
+    <div className="w-full max-w-md mx-auto p-6">
+      <div className="flex items-center mb-4">
+        <label htmlFor="minPrice" className="mr-2">
+          Min Price:
+        </label>
+        <input
+          type="number"
+          id="minPrice"
+          className="border rounded-md py-1 px-2 w-40 mr-4 bg-slate-100 shadow-md"
+          value={minPrice}
+          onChange={handleMinChange}
+        />
       </div>
-      <div className="relative mt-4">
+      <div
+        className="relative h-6 bg-gray-200 rounded-md cursor-pointer"
+        ref={sliderRef}
+        onMouseDown={handleSliderMouseDown}
+        onMouseMove={handleSliderMouseMove}
+        onMouseUp={handleSliderMouseUp}
+        onMouseLeave={handleSliderMouseUp}
+      >
+        <div
+          className="absolute top-0 left-0 h-full bg-blue-500 rounded-md"
+          style={{ width: `${(minPrice / 1000) * 100}%` }}
+        />
         <input
           type="range"
-          min={0}
-          max={1000}
-          step={1}
-          value={sliderValue[0]}
-          onChange={handleSliderChange}
-          className="w-full h-3 bg-gray-300 rounded-full appearance-none outline-none"
+          min="0"
+          max="1000"
+          value={minPrice}
+          className="absolute h-full opacity-0 w-full"
+          style={{ zIndex: 2, cursor: "pointer" }}
+          onChange={handleMinChange}
         />
-        <div
-          className="absolute top-0 left-0 bg-blue-500 w-6 h-6 rounded-full shadow-md transform -translate-x-3 -translate-y-1/2"
-          style={{ left: `${(sliderValue[0] / 1000) * 100}%` }}
-        />
-        <div
-          className="absolute top-0 bg-blue-500 w-6 h-6 rounded-full shadow-md transform -translate-x-3 -translate-y-1/2"
-          style={{ left: `${(sliderValue[1] / 1000) * 100}%` }}
+      </div>
+      <div className="flex items-center mt-4 mb-4">
+        <label htmlFor="maxPrice" className="mr-2">
+          Max Price:
+        </label>
+        <input
+          type="number"
+          id="maxPrice"
+          className="border rounded-md py-1 px-2 w-40 mr-4 bg-slate-100 shadow-md"
+          value={maxPrice}
+          onChange={handleMaxChange}
         />
       </div>
     </div>
--- src/Components/ProductsSidebar.tsx
+++ src/Components/ProductsSidebar.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { FaAngleLeft } from "react-icons/fa";
+import PriceRangeSlider from "./PriceRangeSlider";
 
 const ProductsSidebar: FC = () => {
   return (
@@ -25,6 +26,28 @@ const ProductsSidebar: FC = () => {
         </li>
       </ul>
       <h2 className="text-3xl text-slate-500 mt-16">قیمت</h2>
+      <span className="block h-px w-full bg-slate-300 mt-6"></span>
+      <PriceRangeSlider />
+      <h2 className="text-3xl text-slate-500 mt-16">سایز</h2>
+      <span className="block h-px w-full bg-slate-300 mt-6"></span>
+      <div className="flex justify-center items-center gap-4 mt-6 text-blue-500 text-3xl font-bold">
+        <div className="bg-purple-200/80 p-4 rounded-xl shadow-md w-16 h-16 flex justify-center items-center ">
+          XL
+        </div>
+        <div className="bg-purple-200/80 p-4 rounded-xl shadow-md w-16 h-16 flex justify-center items-center ">
+          L
+        </div>
+        <div className="bg-purple-200/80 p-4 rounded-xl shadow-md w-16 h-16 flex justify-center items-center text-white bg-purple-700">
+          M
+        </div>
+        <div className="bg-purple-200/80 p-4 rounded-xl shadow-md w-16 h-16 flex justify-center items-center ">
+          S
+        </div>
+        <div className="bg-purple-200/80 p-4 rounded-xl shadow-md w-16 h-16 flex justify-center items-center ">
+          SM
+        </div>
+      </div>
+      
     </div>
   );
 };
