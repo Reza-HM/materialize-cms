fc88747d98d8f0f7e025fbdf7d16b26fe28812db
Reza Hamzei <hamzeiamir205@gmail.com>
2023-12-20 10:14:17 +0330

    coding breadcrumb component

---
+++ src/Components/BreadCrumb.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import { FaAngleLeft } from "react-icons/fa";
+
+interface BreadCrumbProps {
+  title: string;
+}
+
+const BreadCrumb: FC<BreadCrumbProps> = ({ title }) => {
+  return (
+    <div className="flex items-center justify-between bg-[url('/img/breadcrumb-bg.jpg')] bg-center bg-cover p-10">
+      <h2 className="text-3xl font-bold text-white">{title}</h2>
+      <div className="flex items-center gap-4 text-slate-300">
+        <p>خانه</p>
+        <FaAngleLeft />
+        <p>برنامه‌ها</p>
+        <FaAngleLeft />
+        <p>{title}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BreadCrumb;
--- src/Pages/Products.tsx
+++ src/Pages/Products.tsx
@@ -1,13 +1,12 @@
-const Products = () => {
+import { FC } from "react";
+import BreadCrumb from "../Components/BreadCrumb";
+
+const Products: FC = () => {
   return (
     <div>
-      <div className="">SS</div>
-      <div className="">SS</div>
-      <div className="">SS</div>
-      <div className="">SS</div>
-      <div className="">SS</div>
-      <div className="">SS</div>
+      <BreadCrumb title="صفحه محصولات تجارت الکترونیکی" />
     </div>
   );
 };
+
 export default Products;
