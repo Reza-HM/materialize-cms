93ae87eb5e291a05a683b709df63d5dbdf474472
Reza Hamzei <hamzeiamir205@gmail.com>
2023-12-25 10:35:11 +0330

    continue coding Questions Page

--- src/Pages/Questions.tsx
+++ src/Pages/Questions.tsx
@@ -1,3 +1,6 @@
+import { FaClipboard, FaSearch, FaUser } from "react-icons/fa";
+import { MdMessage } from "react-icons/md";
+
 const Questions = () => {
   return (
     <div className="mt-8 p-8">
@@ -12,12 +15,43 @@ const Questions = () => {
           placeholder="شروع به تایپ جستجوی خود کنید..."
         />
       </div>
-      <div className="flex items-center gap-8">
-        <div className="flex-1"></div>
-        <div className="flex-1"></div>
-        <div className="flex-1"></div>
-        <div className="flex-1"></div>
-        <div className="flex-1"></div>
+      <div className="flex items-center gap-8 mt-8">
+        <div className="flex-1 flex flex-col items-center gap-8 bg-gray-200 rounded-lg shadow-lg p-12">
+          <FaSearch className="text-7xl font-bold text-yellow-500" />
+          <div className="flex flex-col gap-4 items-center mt-4">
+            <h2 className="text-3xl font-bold">راهنما</h2>
+            <p className="text-slate-500 text-2xl">
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+        </div>
+        <div className="flex-1 flex flex-col items-center gap-8 bg-gray-200 rounded-lg shadow-lg p-12">
+          <MdMessage className="text-7xl font-bold text-red-600" />
+          <div className="flex flex-col gap-4 items-center mt-4">
+            <h2 className="text-3xl font-bold">سوالات متداول</h2>
+            <p className="text-slate-500 text-2xl">
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+        </div>
+        <div className="flex-1 flex flex-col items-center gap-8 bg-gray-200 rounded-lg shadow-lg p-12">
+          <FaUser className="text-7xl font-bold text-green-600" />
+          <div className="flex flex-col gap-4 items-center mt-4">
+            <h2 className="text-3xl font-bold">جامعه</h2>
+            <p className="text-slate-500 text-2xl">
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+        </div>
+        <div className="flex-1 flex flex-col items-center gap-8 bg-gray-200 rounded-lg shadow-lg p-12">
+          <FaClipboard className="text-7xl font-bold text-blue-400" />
+          <div className="flex flex-col gap-4 items-center mt-4">
+            <h2 className="text-3xl font-bold">گزارشات</h2>
+            <p className="text-slate-500 text-2xl">
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
