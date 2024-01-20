427bbb4194b81ca4ff45baf20afca02af7217bc4
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-20 18:07:04 +0330

    changed fetched urls

---
+++ frontend/src/Pages/Products.tsx
@@ -0,0 +1,113 @@
+import { FC, useEffect, useState } from "react";
+import BreadCrumb from "../Components/BreadCrumb";
+import ProductsSidebar from "../Components/ProductsSidebar";
+import ProductBox from "../Components/ProductBox";
+import { FaRegHeart } from "react-icons/fa";
+
+interface ProductRating {
+  rate: number;
+  count: number;
+}
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: ProductRating;
+}
+
+const Products: FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch(`https://materialize-server.liara.run/products`);
+        const productsData: Product[] = await res.json();
+        setProducts(productsData);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <BreadCrumb title="صفحه محصولات تجارت الکترونیکی" />
+      <div className="mt-4 flex gap-8 p-8">
+        <ProductsSidebar />
+        <div className="flex-[3]">
+          <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 gap-8">
+            {products.map((product) => (
+              <ProductBox {...product} key={product.id} />
+            ))}
+          </div>
+          <div className="relative flex flex-wrap gap-8 bg-white text-slate-400 p-8 rounded-lg shadow-lg overflow-hidden mt-12 animate-fade-up">
+            <div
+              className="absolute top-4 left-[-10rem] text-white text-xl flex justify-center items-center text-center w-[25rem] h-[2em] -rotate-45"
+              style={{
+                background: "linear-gradient(-45deg, #8e24aa 0%, #ff6e40 100%)",
+              }}
+            >
+              موجود
+            </div>
+            <div className="xs:flex-1">
+              <img src="/img/remote.png" alt="" />
+            </div>
+            <div className="xs:flex-1">
+              <div className="flex flex-col gap-6 p-4">
+                <p>پاوربانک</p>
+                <h2 className="text-black text-4xl">دسته بازی</h2>
+                <div className="flex gap-4 items-center">
+                  <div className="bg-blue-500 py-1 px-2 rounded-md text-white text-lg">
+                    4.2 امتیاز
+                  </div>
+                  <p>
+                    دسترسی: <span className="text-green-500">موجود</span>
+                  </p>
+                </div>
+                <span className="block h-px w-full bg-slate-300"></span>
+                <div className="flex items-center gap-4">
+                  <FaRegHeart className="text-3xl font-bold" />
+                  <p>لیست علاقه مندی</p>
+                </div>
+                <ul className="list-disc mr-8">
+                  <li>لیست علاقه مندی</li>
+                  <li>سازگار با تمام تلفن های هوشمند</li>
+                  <li>طراحی قابل حمل و وزن سبک</li>
+                  <li>نوع باتری : یون لیتیوم</li>
+                </ul>
+                <div className="flex items-center gap-4">
+                  <p className="text-red-500 text-4xl font-bold">
+                    ۷۹۰۰۰ تومان{" "}
+                  </p>
+                  <p className="text-4xl font-bold">۱۹۹۰۰۰ تومان</p>
+                </div>
+                <div className="flex flex-col gap-4">
+                  <button className="bg-blue-600 py-2 px-4 w-72 rounded-lg text-white shadow-xl shadow-zinc-500 hover:shadow-sm transition-all duration-300">
+                    اضافه به سبد خرید{" "}
+                  </button>
+                  <button
+                    className="py-2 px-4 w-80 rounded-lg text-white shadow-xl shadow-zinc-500 hover:shadow-sm transition-all duration-300"
+                    style={{
+                      background: "linear-gradient(-45deg, #8e24aa, #ff6e40)",
+                    }}
+                  >
+                    هم اکنون خریداری کنید{" "}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
---
+++ frontend/src/Pages/Questions.tsx
@@ -0,0 +1,146 @@
+import { FC, useEffect, useState } from "react";
+import { FaClipboard, FaSearch, FaUser } from "react-icons/fa";
+import { MdMessage } from "react-icons/md";
+import Accordion from "../Components/Accordion";
+
+interface Question {
+  id: number;
+  question: string;
+  answer: string;
+}
+
+const Questions: FC = () => {
+  const [questions, setQuestions] = useState<Question[]>([]);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const res = await fetch(`https://materialize-server.liara.run/questions`);
+        const questionsData: Question[] = await res.json();
+        setQuestions(questionsData);
+      } catch (error) {
+        console.error("Error fetching questions:", error);
+      }
+    };
+
+    fetchQuestions();
+  }, []);
+
+  return (
+    <div className="mt-8 p-8">
+      <div className="bg-[url('/img/faq-search.jpg')] bg-cover bg-no-repeat p-24 rounded-lg shadow-lg flex flex-col items-center gap-8 animate-fade-up text-center">
+        <h2 className="text-zinc-700 text-4xl font-bold">سوالات متداول</h2>
+        <p className="text-red-500 font-bold md:text-slate-500">
+          برای پاسخ‌های سریع، مرکز راهنمایی ما را جستجو کنید
+        </p>
+        <input
+          type="text"
+          className="rounded-full mt-8 p-6 w-full shadow-md shadow-zinc-700 text-slate-500 text-2xl"
+          placeholder="شروع به تایپ جستجوی خود کنید..."
+        />
+      </div>
+      <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-4 text-center gap-8 mt-8 animate-fade-up">
+        <div className="flex flex-col items-center gap-8 bg-gray-200 rounded-lg shadow-lg p-12">
+          <FaSearch className="text-7xl font-bold text-yellow-500" />
+          <div className="flex flex-col gap-4 items-center mt-4">
+            <h2 className="text-3xl font-bold">راهنما</h2>
+            <p className="text-slate-500 text-2xl">
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-8 bg-gray-200 rounded-lg shadow-lg p-12">
+          <MdMessage className="text-7xl font-bold text-red-600" />
+          <div className="flex flex-col gap-4 items-center mt-4">
+            <h2 className="text-3xl font-bold">سوالات متداول</h2>
+            <p className="text-slate-500 text-2xl">
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-8 bg-gray-200 rounded-lg shadow-lg p-12">
+          <FaUser className="text-7xl font-bold text-green-600" />
+          <div className="flex flex-col gap-4 items-center mt-4">
+            <h2 className="text-3xl font-bold">جامعه</h2>
+            <p className="text-slate-500 text-2xl">
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-8 bg-gray-200 rounded-lg shadow-lg p-12">
+          <FaClipboard className="text-7xl font-bold text-blue-400" />
+          <div className="flex flex-col gap-4 items-center mt-4">
+            <h2 className="text-3xl font-bold">گزارشات</h2>
+            <p className="text-slate-500 text-2xl">
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="mt-12 flex flex-wrap gap-8">
+        <div className="w-full md:flex-1 flex flex-col gap-16 bg-white rounded-lg shadow-md shadow-zinc-400 p-8 animate-fade-left">
+          <div className="">
+            <h2 className="text-3xl text-slate-600 mb-8">دسته بندی</h2>
+            <ul className="flex flex-col gap-4 text-slate-500">
+              <li className="flex items-center gap-4">
+                <span className="bg-transparent border-2 border-yellow-500 rounded-[100%] w-8 h-8"></span>
+                برنامه های کاربردی
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="bg-transparent border-2 border-blue-500 rounded-[100%] w-8 h-8"></span>
+                عناصر رابط کاربری
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="bg-transparent border-2 border-purple-800 rounded-[100%] w-8 h-8"></span>
+                اجزا
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="bg-transparent border-2 border-green-500 rounded-[100%] w-8 h-8"></span>
+                فرآیند ساخت
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <h2 className="text-3xl text-slate-600 mb-8">فعالان</h2>
+            <ul className="flex flex-col gap-4">
+              <li className="flex items-center gap-4">
+                <img src="/img/2.jpg" className="w-16 h-16" alt="" />
+                <div className="">
+                  <h4 className="text-blue-500">مایک</h4>
+                  <p className="text-slate-500">مایکروسافت</p>
+                </div>
+              </li>
+              <li className="flex items-center gap-4">
+                <img src="/img/2.jpg" className="w-16 h-16" alt="" />
+                <div className="">
+                  <h4 className="text-blue-500">مایک</h4>
+                  <p className="text-slate-500">مایکروسافت</p>
+                </div>
+              </li>
+              <li className="flex items-center gap-4">
+                <img src="/img/2.jpg" className="w-16 h-16" alt="" />
+                <div className="">
+                  <h4 className="text-blue-500">مایک</h4>
+                  <p className="text-slate-500">مایکروسافت</p>
+                </div>
+              </li>
+              <li className="flex items-center gap-4">
+                <img src="/img/2.jpg" className="w-16 h-16" alt="" />
+                <div className="">
+                  <h4 className="text-blue-500">مایک</h4>
+                  <p className="text-slate-500">مایکروسافت</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="md:flex-[4] flex flex-col gap-4 animate-fade-right">
+          {questions.map((question) => (
+            <Accordion {...question} key={question.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Questions;
